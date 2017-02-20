b0c8649f49bb235f6a7d74c1bce0a25844705b34
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-02-20 11:22:28 +0530

    First commit

---
+++ catch-of-the-day/src/components/Inventory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Inventory extends React.Component {
+	render() {
+		return (
+			<p>Inventory</p>
+
+		)
+	}
+}
+
+export default Inventory;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Order extends React.Component {
+	render() {
+		return (
+			<p>Order</p>
+
+		)
+	}
+}
+
+export default Order;
