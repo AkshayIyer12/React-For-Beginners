c3f58f44450725fc43037553bba4dfb2c059301f
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-02-22 11:25:38 +0530

    Added Miss component to the BrowserRouter for NotFound state

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -7,13 +7,15 @@ import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
 const Root = () => {
 	return (
 		<BrowserRouter>
 		<div>
 			<Match exactly pattern="/" component={StorePicker} />
-			<Match exactly pattern="/store/:storeId" component={App} />
+        	<Match pattern="/store/:storeId" component={App} />
+        	<Miss component={NotFound} />
 		</div>
 			
 		</BrowserRouter>
