23f2ccadf973a2a2d21725f65530f9d311449ab2
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-02-22 23:22:01 +0530

    Created a state component; Need to revise it again and go through the facebook.github.io documentation on states

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -5,6 +5,28 @@ import Inventory from './Inventory';
 
 
 class App extends React.Component {
+
+	constructor() {
+		super();
+		// getinitalState
+		this.addFish = this.addFish.bind(this);
+		this.state = {
+			fishes: {},
+			order: {}
+		};
+	}
+
+	addFish(fish){
+		//update our state
+		const fishes = {...this.state.fishes};
+		//add in our new fish
+		const timeStamp = Date.now();
+		fishes[`fish-${timeStamp}`] = fish;
+		// this.state.fishes.fish1 = fish;
+		//set state
+		this.setState({ fishes: fishes })
+	}
+
 	render() {
 		return (
 		<div className="catch-of-the-day">
@@ -12,7 +34,7 @@ class App extends React.Component {
 				<Header tagline="Fresh Seafood Market" />
 			</div>
 			<Order />
-			<Inventory />
+			<Inventory addFish={this.addFish} />
 		</div>
 		)
 	}
