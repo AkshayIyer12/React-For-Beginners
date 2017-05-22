c8f135e623964d77f667b9ff5270b9e05226c9f0
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-05-22 20:06:23 +0530

    Updating the order state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
 		// getinitalState
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		this.state = {
 			fishes: {},
 			order: {}
@@ -33,7 +34,14 @@ class App extends React.Component {
 			fishes: sampleFishes
 		});
 	}
-	addToOrder
+	addToOrder(key) {
+		//take a copy of our state
+		const order = {...this.state.order};
+		//update or add the new number of fish ordered 
+		order[key] = order[key] + 1 || 1;
+		//update our state
+		this.setState({ order });
+	}
 	render() {
 		return (
 		<div className="catch-of-the-day">
@@ -43,7 +51,7 @@ class App extends React.Component {
 				{
 					Object
 					.keys(this.state.fishes)
-					.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+					.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
 				}
 				
 				</ul>
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 class Fish extends React.Component {
 	render() {
-		const { details } = this.props;
+		const { details, index } = this.props;
 		const isAvailable = details.status === 'available';
 		const buttonText = isAvailable ? 'Add To Order' :'Sold Out!';
 		return(
@@ -13,7 +13,7 @@ class Fish extends React.Component {
 				<span className="price">{formatPrice(details.price)}</span>
 			</h3>
 			<p>{details.desc}</p>
-			<button disabled={!isAvailable} >{ buttonText }</button>
+			<button onClick={ ()=> this.props.addToOrder(index)} disabled={!isAvailable} >{ buttonText }</button>
 		</li>
 		)
 	}
