380c6870dba2348985c683b6ccea16b79c4683ed
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-05-22 22:05:12 +0530

    Displaying Order state with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -56,7 +56,7 @@ class App extends React.Component {
 				
 				</ul>
 			</div>
-			<Order />
+			<Order fishes={this.state.fishes} order={this.state.order}/>
 			<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 		</div>
 		)
