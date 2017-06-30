d801bee8fe5833215d767eb3bae713739b93eaa8
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-07-01 01:04:03 +0530

    Lots of CSS by using animation with React Components

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { formatPrice } from '../helpers';
-
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class Order extends React.Component {
 	constructor() {
@@ -18,7 +18,19 @@ class Order extends React.Component {
 		}
 		
 		return (<li key={key}>
-			<span>{count}lbs {fish.name} {removeButton}</span>
+			<span>
+			 <CSSTransitionGroup
+			 	component="span"
+			 	className="count"
+			 	transitionName="count"
+			 	transitionEnterTimeOut={250}
+			 	transitionLeaveTimeOut={250}
+			 >
+			 	<span key={count} >{count}</span>
+			 </CSSTransitionGroup>
+			 
+			 lbs {fish.name} {removeButton}
+			</span>
 			<span className="price">{formatPrice(count * fish.price)}</span>
 		</li>
 
@@ -38,13 +50,18 @@ class Order extends React.Component {
 		return (
 			<div className="order-wrap">
 				<h2>Your Order</h2>
-				<ul className="order">
+
+				<CSSTransitionGroup className="order" 
+				component="ul"
+				transitionName="order"
+				transitionEnterTimeOut={250}
+				transitionLeaveTimeOut={250}>
 					{orderIds.map(this.renderOrder)}
 					<li className="total">
 						<strong>Total:</strong>
 						{formatPrice(total)}
 					</li>
-				</ul>
+				</CSSTransitionGroup>
 				
 			</div>
 
